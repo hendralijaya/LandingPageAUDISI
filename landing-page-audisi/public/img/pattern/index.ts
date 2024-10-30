019679bcdbd9800a672d53a988d837f46ecb0ad9
Hendra Lijaya <hendralijaya@gmail.com>
2024-10-30 21:38:55 +0700

    push static file systems import

--- landing-page-audisi/public/img/pattern/index.ts
+++ landing-page-audisi/public/img/pattern/index.ts
@@ -1,5 +1,4 @@
 import PatternMobile from './pattern-mobile.png';
 import PatternTablet from './pattern-tablet.png';
 import PatternDesktop from './pattern-desktop.png';
-
 export { PatternMobile, PatternTablet, PatternDesktop };
